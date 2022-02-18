93687999fe3123bc7f6158ce8c3eccf413f52225
cmr211 <bartosz.surma211@gmail.com>
2022-02-18 15:01:28 +0100

    day 7

--- day-7.mjs
+++ day-7.mjs
@@ -1,20 +1,68 @@
-// import { input } from "./inputs/day-7-input.mjs";
+import { input } from "./inputs/day-7-input.mjs"
 
-const test = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+// solve7b(input)
+// Optimal Position: 497 | Fuel Consumption: 100727924
 
-function average(array) {
-    return array.reduce((a, b) => a + b) / array.length;
+// solve7a(input)
+// -> Optimal Position: 346 | Fuel Consumption: 359648
+
+function solve7b(data) {
+    const max = Math.max(...data)
+    const consumptions = []
+    for (let target = 0; target <= max; target++) {
+        const fuel = data
+            .map(n => specialFuelConsumption(n, target))
+            .reduce((a, b) => a + b)
+        consumptions.push(fuel)
+    }
+    const minConsumptions = Math.min(...consumptions)
+    const optPos = consumptions.indexOf(minConsumptions)
+    console.log(
+        "Optimal Position:",
+        optPos,
+        "| Fuel Consumption:",
+        minConsumptions
+    )
 }
 
-Array.prototype.quickSort = () => quickSort(this)
+function specialFuelConsumption(pos, target) {
+    const dist = Math.abs(pos - target)
+    return (dist ** 2 + dist) / 2
+}
 
-function quickSort(array) {
-    if (array.length < 2) return array;
-    const pivot = array.pop();
-    const left = array.filter(item => item < pivot);
-    const right = array.filter(item => item >= pivot);
+function solve7a(input) {
+    const optimalPosition = customMedian(input)
+    const fuelConsumption = flatFuelConsumption(input, optimalPosition)
+    console.log(
+        "Optimal Position:",
+        optimalPosition,
+        "| Fuel Consumption:",
+        fuelConsumption
+    )
+    return fuelConsumption
+}
+
+function flatFuelConsumption(array, target) {
+    const a = [...array]
+    return a.map(i => Math.abs(i - target)).reduce((a, b) => a + b)
+}
+
+function customMedian(input) {
+    const array = [...input]
+    const sorted = quickSort(array)
+    if (sorted.length % 2 === 0) {
+        return sorted[sorted.length / 2 - 1]
+    } else {
+        return sorted[Math.floor(sorted.length / 2)]
+    }
+}
+
+function quickSort(input) {
+    const array = [...input]
+    if (array.length < 2) return array
+    const pivot = array.pop()
+    const left = array.filter(item => item < pivot)
+    const right = array.filter(item => item >= pivot)
     // console.log("Left:", left, "| Pivot:", pivot, "| Right:", right);
-    return [...quickSort(left), pivot, ...quickSort(right)];
+    return [...quickSort(left), pivot, ...quickSort(right)]
 }
-
-console.log(test.quickSort())
